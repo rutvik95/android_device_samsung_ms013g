36200ec3fd6ad1862751bea342c6608b8af47a2a
Rutvik Rajagopal <rutvik_95@yahoo.co.in>
2014-06-29 18:28:33 +0530

    added ms013g kernel config changed falcon device according to ms013g changes to build from msm8226-common tree not required

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,57 +1,24 @@
-USE_CAMERA_STUB := true
+-include device/samsung/msm8226-common/BoardConfigCommon.mk
 
 # inherit from the proprietary version
 -include vendor/samsung/ms013g/BoardConfigVendor.mk
 
-TARGET_ARCH := arm
-TARGET_NO_BOOTLOADER := true
-TARGET_BOARD_PLATFORM := msm8226
-TARGET_CPU_ABI := armeabi-v7a
-TARGET_CPU_ABI2 := armeabi
-TARGET_ARCH_VARIANT := armv7-a-neon
-TARGET_CPU_VARIANT := krait
-ARCH_ARM_HAVE_TLS_REGISTER := true
+LOCAL_PATH := device/samsung/ms013g
 
-TARGET_BOOTLOADER_BOARD_NAME := ms013g
+# Vendor Init
+TARGET_UNIFIED_DEVICE := true
+TARGET_INIT_VENDOR_LIB := libinit_msm
 
-TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
-TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
 
-# Kernel
-TARGET_PREBUILT_KERNEL := device/samsung/ms013g/kernel
-TARGET_KERNEL_SOURCE := kernel/samsung/msm8226
-TARGET_KERNEL_CONFIG := cyanogenmod_ms013g_defconfig
-BOARD_KERNEL_CMDLINE := console=null androidboot.console=null androidboot.hardware=qcom user_debug=31 maxcpus=4 msm_rtb.filter=0x3F
-BOARD_KERNEL_BASE := 0x00000000
-BOARD_KERNEL_PAGESIZE := 2048
-BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000 --dt device/samsung/ms013g/dtb --tags_offset 0x01e00000
+# Custom relese tools for unified devices
+TARGET_RELEASETOOLS_EXTENSIONS := device/samsung/ms013g
 
-# fix this up by examining /proc/mtd on a running device
-BOARD_BOOTIMAGE_PARTITION_SIZE := 0x105c0000
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x105c0000
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x105c0000
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x105c0000
-BOARD_FLASH_BLOCK_SIZE := 131072
-TARGET_USERIMAGES_USE_EXT4 := true
-
-# Recovery
-BOARD_HAS_NO_SELECT_BUTTON := true
-TARGET_RECOVERY_FSTAB := device/samsung/ms013g/ramdisk/root/fstab.qcom
-#BOARD_RECOVERY_SWIPE :=true
-#TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
-BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_23x41.h\"
-BOARD_HAS_LARGE_FILESYSTEM := true
-BOARD_HAS_NO_MISC_PARTITION := true
-BOARD_HAS_DOWNLOAD_MODE := true
-BOARD_USES_MMCUTILS := true
+#bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(LOCAL_PATH)/bluetooth
 
 #TWRP
-#DEVICE_RESOLUTION := 720x1280
-#RECOVERY_SDCARD_ON_DATA := true
-#TW_INTERNAL_STORAGE_PATH := "/data/media/0"
-#TW_INTERNAL_STORAGE_MOUNT_POINT := "data"
-#TW_EXTERNAL_STORAGE_PATH := "/external_sd"
-#TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
-#TW_HAS_DOWNLOAD_MODE := true
-#RECOVERY_GRAPHICS_USE_LINELENGTH := true
-#TW_IGNORE_MAJOR_AXIS_0 := true
+DEVICE_RESOLUTION := 720x1280
+TW_IGNORE_MAJOR_AXIS_0 := true
+
+# userdata 8GB
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 8589934592
--- device_ms013g.mk
+++ device_ms013g.mk
@@ -1,49 +1,18 @@
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-
-# The gps config appropriate for this device
-$(call inherit-product, device/common/gps/gps_us_supl.mk)
-
-$(call inherit-product-if-exists, vendor/samsung/ms013g/ms013g-vendor.mk)
-$(call inherit-product, device/samsung/msm8226-common/keylayout/keylayout.mk)
-
-DEVICE_PACKAGE_OVERLAYS += device/samsung/ms013g/overlay
-
-LOCAL_PATH := device/samsung/ms013g
-ifeq ($(TARGET_PREBUILT_KERNEL),)
-	LOCAL_KERNEL := $(LOCAL_PATH)/kernel
-else
-	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
-endif
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_KERNEL):kernel
-
-# Ramdisk
-PRODUCT_PACKAGES += \
-	fstab.qcom \
-
-# Philz
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/extra.fstab:recovery/root/etc/extra.fstab
-
-PRODUCT_LOCALES := en_US
-PRODUCT_LOCALES += xhdpi
-PRODUCT_AAPT_CONFIG := normal hdpi xhdpi
-PRODUCT_AAPT_PREF_CONFIG := xhdpi
-
-
 $(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 $(call inherit-product, device/samsung/msm8226-common/msm8226.mk)
 
-# TWRP
-#PRODUCT_COPY_FILES += \
-#    $(LOCAL_PATH)/twrp.fstab:recovery/root/etc/twrp.fstab
+LOCAL_PATH := device/samsung/ms013g
 
-$(call inherit-product, build/target/product/full.mk)
+# ms013g specific overlay
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
-PRODUCT_NAME := full_ms013g
-PRODUCT_DEVICE := ms013g
+PRODUCT_LOCALES := en_US
+PRODUCT_LOCALES += xhdpi
+PRODUCT_AAPT_CONFIG := normal hdpi xhdpi
+PRODUCT_AAPT_PREF_CONFIG := xhdpi
+
+$(call inherit-product, device/samsung/msm8226-common/keylayout/keylayout.mk)
+$(call inherit-product, vendor/samsung/ms013g/ms013g-vendor.mk)
